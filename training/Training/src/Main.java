c7f39c29fa849922cceaad9151976fec1a6e9bb7
RA1AIO <ra1aio@ya.ru>
2022-06-03 23:29:56 +0300

    h/z

--- training/Training/src/Main.java
+++ training/Training/src/Main.java
@@ -1,100 +1,40 @@
+import java.sql.Array;
 import java.util.Scanner;
-//import java.util.Random;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        double[] expenses = new double[7];
 
-        double rateUSD = 78.5;
-        double rateEUR = 85;
-        double rateJPY = 0.74;
+        Random random = new Random();
+        int arrayLength = random.nextInt(4, 5); // Гнерим размер массива
 
-        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[arrayLength];
+        System.out.println("Размер массива = " + array.length);
 
-        System.out.println("Сколько денег у вас осталось до зарплаты?");
-        double moneyBeforeSalary = scanner.nextDouble();
-
-        System.out.println("Сколько дней до зарплаты?");
-        int daysBeforeSalary = scanner.nextInt();
-
-        while (true) {
-            System.out.println("Что вы хотите сделать?");
-            System.out.println("1 - Конвертировать валюту");
-            System.out.println("2 - Получить совет");
-            System.out.println("3 - Ввести трату");
-            System.out.println("4 - Показать траты за неделю");
-            System.out.println("5 - Показать самую большую сумму расходов за неделю");
-            System.out.println("0 - Выход");
-
-            int command = scanner.nextInt();
-
-            if (command == 1) {
-                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
-                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
-                int currency = scanner.nextInt();
-                if (currency == 1) {
-                    System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
-                } else if (currency == 2) {
-                    System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
-                } else if (currency == 3) {
-                    System.out.println("Ваши сбережения в иенах: " + moneyBeforeSalary / rateJPY);
-                } else {
-                    System.out.println("Неизвестная валюта");
-                }
-            } else if (command == 2) {
-                if (moneyBeforeSalary < 3000) {
-                    System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-                } else if (moneyBeforeSalary < 10000){
-                    if (daysBeforeSalary < 10) {
-                        System.out.println("Окей, пора в Макдак!");
-                    } else {
-                        System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-                    }
-                } else if (moneyBeforeSalary < 30000) {
-                    if (daysBeforeSalary < 10) {
-                        System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
-                    } else {
-                        System.out.println("Окей, пора в Макдак!");
-                    }
-                } else {
-                    if (daysBeforeSalary < 10) {
-                        System.out.println("Отлично! Заказывайте крабов!");
-                    } else {
-                        System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
-                    }
-                }
-            } else if (command == 3) {
-                System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
-                int day = scanner.nextInt();
-                System.out.println("Введите размер траты:");
-                double expense = scanner.nextDouble();
-                moneyBeforeSalary = moneyBeforeSalary - expense;
-                expenses[day - 1] = expenses[day - 1] + expense;
-                System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
-                if (moneyBeforeSalary < 1000) {
-                    System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
-                }
-            } else if (command == 4) {   // Добавьте ветвление для обработки новой команды
-                for (int i = 0; i < expenses.length; i++) {     // Используйте цикл for, чтобы получить все траты — элементы массива expenses
-                    // Напечатайте в цикле строку: "День _. Потрачено _ рублей.".
-                    System.out.println("День " + (i + 1) + ". " + "Потрачено " + expenses[i] + " рублей.");
-                }
-            }else if (command == 5){
-                    double maxExpense=0;
-
-                    for (int i = 0; i < expenses.length; i ++){
-                        if (expenses[i] > maxExpense){
-                            maxExpense = expenses[i];
-                        }
-                    }
-                    System.out.println("Самая большая сумма расходов на этой неделе составила " + maxExpense + " руб.");
-            }
-            else if (command == 0) {
-                System.out.println("Выход");
-                break;
-        } else {
-            System.out.println("Извините, такой команды пока нет.");
+        for (int i = 0; i < array.length; i++){
+            int randomInt = random.nextInt(1000);
+            array[i] = randomInt;
+            System.out.println("Элемент[" + i + "] = " + array[i]);
         }
-    }
+        int min = array[0];
+        int max = array[0];
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < min ){
+                min = array[i];
+            } else if (array[i] > max) {
+                max = array[i];
+            }
+
+            sum = sum + array[i];
+        }
+        System.out.println("Min = " + min);
+        System.out.println("Max = " + max);
+        System.out.println("Avg = " + sum / array.length);
+
+
+
+
     }
 }
